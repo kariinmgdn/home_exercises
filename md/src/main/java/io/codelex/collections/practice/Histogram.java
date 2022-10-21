6ab2a4a35e07838f283773106f3a6454ecb52047
Karīna Dzene <karina.dzene@gmail.com>
2022-10-21 13:53:30 +0300

    8. uzd

--- md/src/main/java/io/codelex/collections/practice/Histogram.java
+++ md/src/main/java/io/codelex/collections/practice/Histogram.java
@@ -6,6 +6,8 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
 
 public class Histogram {
     private static final Charset charset = Charset.defaultCharset();
@@ -13,7 +15,22 @@ public class Histogram {
 
     public static void main(String[] args) throws IOException, URISyntaxException {
         final String scores = fileContent();
-        System.out.println(scores);
+        List<Integer> intScores = Arrays.stream(scores.split(" ")).map(Integer::parseInt).toList();
+        for (int i = 0; i <= 100; i += 10) {
+            int max = i + 9;
+            int count = 0;
+            for (Integer score : intScores) {
+                if (score <= max && score >= i) {
+                    count++;
+                }
+            }
+            String output = String.format("%02d", i) + "-" + String.format("%02d", max) + ": " + "*".repeat(count);
+            if (i == 100) {
+                output = "  100: " + "*".repeat(count);
+            }
+            System.out.println(output);
+        }
+
     }
 
     private static String fileContent() throws IOException {
